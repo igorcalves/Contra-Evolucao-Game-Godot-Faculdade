07b49dd2249acc5ab7488dbf48a82a969093299d
victor4624 <victor.yip.paulino@gmail.com>
2023-05-29 12:14:10 -0300

    Learn Godot

---
+++ Learn Godot/Scripts/Global.gd
@@ -0,0 +1,8 @@
+extends Node
+
+var coins = 0
+var kills = 0
+var health = 10
+
+func _ready():
+	pass
---
+++ Learn Godot/level_reset.gd
@@ -0,0 +1,6 @@
+extends Node
+
+func _ready():
+	Global.coins = 0
+	Global.kills = 0
+	Global.health = 10
